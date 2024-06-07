fdc652e29b37b5648684c7cf53c4c02943b2687a
System Administrator <root@songtaejeong-ui-MacBookAir.local>
2024-06-07 18:43:52 +0900

    site.ts 수정

--- config/site.ts
+++ config/site.ts
@@ -1,29 +1,29 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "let's do this!",
+  description: "송태정 포트폴리오 사이트",
   navItems: [
     {
       label: "Home",
       href: "/",
     },
-    {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
-    },
+    // {
+    //   label: "Docs",
+    //   href: "/docs",
+    // },
+    // {
+    //   label: "Pricing",
+    //   href: "/pricing",
+    // },
+    // {
+    //   label: "Blog",
+    //   href: "/blog",
+    // },
+    // {
+    //   label: "About",
+    //   href: "/about",
+    // },
     {
       label: "Todos",
       href: "/todos",
